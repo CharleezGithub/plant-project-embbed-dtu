d740dc3840abc77859493f1ee353ab3b6675ab38
Charlee <charleegithub@gmail.com>
2025-01-10 13:58:59 +0100

    Renamed main and cleaned out, ready to write in

---
+++ plant-project/main/main.c
@@ -0,0 +1,11 @@
+#include "freertos/FreeRTOS.h" // FreeRTOS base functionality
+#include "freertos/task.h"     // Task creation and delay functions
+#include "driver/gpio.h"       // GPIO control and configuration
+#include "esp_system.h"        // ESP32 system-related functions
+#include "esp_log.h"           // Logging utilities
+#include "esp_timer.h"         // High-resolution timer API
+#include "sdkconfig.h"         // Project configuration
+
+void app_main()
+{
+}
